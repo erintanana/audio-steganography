57ed39effcc61e65d678f0b15dbd5430dbb3bb71
Erin Erin <aryna.tanana@outlook.com>
2020-05-24 11:12:06 +0300

    Upload audio file and embedding form

---
+++ Steganography.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Steganography.Web.Startup))]
+namespace Steganography.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
